53fdd1e59f17b82576281dc2889e04356ef32968
Frank Fella <frank@techelevator.com>
2018-10-10 20:08:57 -0400

    Instructor Push

---
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -0,0 +1,41 @@
+package com.techelevator;
+
+import com.techelevator.view.Menu;
+
+public class VendingMachineCLI {
+
+	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
+	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
+	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
+	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS,
+													    MAIN_MENU_OPTION_PURCHASE,
+													    MAIN_MENU_OPTION_EXIT};
+	
+	private Menu menu;
+	
+	public VendingMachineCLI(Menu menu) {
+		this.menu = menu;
+	}
+	
+	public void run() {
+		while(true) {
+			String choice = (String)menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
+			
+			if(choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
+				// display vending machine items
+			} else if(choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
+				// do purchase
+			}
+			  else if(choice.equals(MAIN_MENU_OPTION_EXIT)) {
+				// do any end of program processing
+			       return;	  
+			  }		  
+		}
+	}
+	
+	public static void main(String[] args) {
+		Menu menu = new Menu(System.in, System.out);
+		VendingMachineCLI cli = new VendingMachineCLI(menu);
+		cli.run();
+	}
+}
