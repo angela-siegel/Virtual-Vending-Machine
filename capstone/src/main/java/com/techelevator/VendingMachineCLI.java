863305f4cf06c9533646cee48d1832939e58a71d
Frank Fella <frank@techelevator.com>
2018-10-11 05:54:52 -0400

    Instructor Push

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -4,9 +4,9 @@ import com.techelevator.view.Menu;
 
 public class VendingMachineCLI {
 
-	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
-	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
-	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
+	private static final String   MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
+	private static final String   MAIN_MENU_OPTION_PURCHASE      = "Purchase";
+	private static final String   MAIN_MENU_OPTION_EXIT          = "Exit";
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS,
 													    MAIN_MENU_OPTION_PURCHASE,
 													    MAIN_MENU_OPTION_EXIT};
@@ -18,17 +18,19 @@ public class VendingMachineCLI {
 	}
 	
 	public void run() {
-		while(true) {
+		boolean shouldLoop = true;
+		while(shouldLoop) {
 			String choice = (String)menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 			
 			if(choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-				// display vending machine items
-			} else if(choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-				// do purchase
+				// display vending machine items - good place for a method call
+			} 
+			else if(choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
+				// do purchase - good place for a method call
 			}
-			  else if(choice.equals(MAIN_MENU_OPTION_EXIT)) {
-				// do any end of program processing
-			       return;	  
+			else if(choice.equals(MAIN_MENU_OPTION_EXIT)) {
+				// do any end of program processing - good place for a method call
+		         shouldLoop=false;	  
 			  }		  
 		}
 	}
